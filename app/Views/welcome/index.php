bf8d9708f80423082f7453b8dfe8d7d989265d36
Mark A. Connelly <16519297+markconnellypro@users.noreply.github.com>
2025-11-29 00:30:49 -0500

    Welcome views: Update with new language tags

--- app/Views/welcome/index.php
+++ app/Views/welcome/index.php
@@ -6,25 +6,25 @@ $welcome ??= '';
 <div id="welcomecontainer">
     <div id="welcometext" class="welcomecontent"><?= $welcome ?></div>
     <div id="welcomestate" class="welcomecontent">
-        <a href="/<?= \Config\Services::request()->getLocale() ?>/about/" class="welcomeiconcontainer" aria-label="About">
+        <a href="/<?= \Config\Services::request()->getLocale() ?>/about/" class="welcomeiconcontainer" aria-label="<?= lang('Application.about') ?>">
             <span class="welcomeicon">info</span>
-            <div style="height: auto;">About</div>
+            <div style="height: auto;"><?= lang('Application.about') ?></div>
         </a>
-        <a href="/<?= \Config\Services::request()->getLocale() ?>/key/" class="welcomeiconcontainer" aria-label="Key">
+        <a href="/<?= \Config\Services::request()->getLocale() ?>/key/" class="welcomeiconcontainer" aria-label="<?= lang('Application.key') ?>">
             <span class="welcomeiconfill">vpn_key</span>
-            <div style="height: auto;">Key</div>
+            <div style="height: auto;"><?= lang('Application.key') ?></div>
         </a>
-        <a href="/<?= \Config\Services::request()->getLocale() ?>/search/" class="welcomeiconcontainer" aria-label="Search">
+        <a href="/<?= \Config\Services::request()->getLocale() ?>/search/" class="welcomeiconcontainer" aria-label="<?= lang('Application.search') ?>">
             <span class="welcomeicon">search</span>
-            <div style="height: auto;">Search</div>
+            <div style="height: auto;"><?= lang('Application.search') ?></div>
         </a>
-        <a href="/<?= \Config\Services::request()->getLocale() ?>/statistics/" class="welcomeiconcontainer" aria-label="Statistics">
+        <a href="/<?= \Config\Services::request()->getLocale() ?>/statistics/" class="welcomeiconcontainer" aria-label="<?= lang('Application.statistics') ?>">
             <span class="welcomeicon">insert_chart</span>
-            <div style="height: auto;">Statistics</div>
+            <div style="height: auto;"><?= lang('Application.statistics') ?></div>
         </a>
-        <a href="/<?= \Config\Services::request()->getLocale() ?>/status/" class="welcomeiconcontainer" aria-label="Status">
+        <a href="/<?= \Config\Services::request()->getLocale() ?>/status/" class="welcomeiconcontainer" aria-label="<?= lang('Application.status') ?>">
             <span class="welcomeicon">map</span>
-            <div style="height: auto;">Status</div>
+            <div style="height: auto;"><?= lang('Application.status') ?></div>
         </a>
     </div>
 </div>
