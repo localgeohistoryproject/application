1e9f130a9f70c56ce6a485f6e8e37a87ecc14846
Mark A. Connelly <16519297+markconnellypro@users.noreply.github.com>
2025-11-28 23:23:09 -0500

    Government Source views: Update with new language tags

--- app/Views/governmentsource/table.php
+++ app/Views/governmentsource/table.php
@@ -3,21 +3,21 @@
     $type ??= '';
     ?>
 <section>
-    <h2>Government Action</h2>
+    <h2><?= lang('Application.governmentAction') ?></h2>
     <table class="normal cell-border compact stripe">
         <thead>
             <tr>
                 <?php if ($type !== 'source') { ?>
-                    <th>Detail</th>
+                    <th><?= lang('Application.detail') ?></th>
                 <?php }
                 if ($type !== 'government' || $isMultiple) { ?>
-                    <th>Government</th>
+                    <th><?= lang('Application.government') ?></th>
                 <?php } ?>
-                <th>Action</th>
-                <th>Date</th>
-                <th>Approved</th>
-                <th>Effective</th>
-                <th>Location</th>
+                <th><?= lang('Application.action') ?></th>
+                <th><?= lang('Application.date') ?></th>
+                <th><?= lang('Application.approved') ?></th>
+                <th><?= lang('Application.effective') ?></th>
+                <th><?= lang('Application.location') ?></th>
             </tr>
         </thead>
         <tbody>
@@ -26,11 +26,11 @@
                     <?php if ($type !== 'source') { ?>
                         <td data-sort="<?= $row->governmentsourcesort ?>">
                             <?php if (isset($row->governmentsourceslug) && $row->governmentsourceslug !== '') { ?>
-                                <a href="/<?= \Config\Services::request()->getLocale() ?>/governmentsource/<?= $row->governmentsourceslug ?>/">View</a>
+                                <a href="/<?= \Config\Services::request()->getLocale() ?>/governmentsource/<?= $row->governmentsourceslug ?>/"><?= lang('Application.view') ?></a>
                                 <?php } elseif (isset($row->eventslug)) {
                                     $i = 0;
                                     foreach (explode(',', str_replace(['{', '}'], '', $row->eventslug)) as $event) { ?>
-                                    <?= ($i > 0 ? '<br>' : '') ?><a href="/<?= \Config\Services::request()->getLocale() ?>/event/<?= $event ?>/">View</a>
+                                    <?= ($i > 0 ? '<br>' : '') ?><a href="/<?= \Config\Services::request()->getLocale() ?>/event/<?= $event ?>/"><?= lang('Application.view') ?></a>
                             <?php $i++;
                                     }
                                 } ?>
@@ -52,7 +52,7 @@
                     <td data-sort="<?= $row->governmentsourcedatesort ?>"><?= $row->governmentsourcedate ?></td>
                     <td data-sort="<?= $row->governmentsourceapproveddatesort ?>">
                         <?= ($row->governmentsourceapproved === 't'
-                            ? (($row->governmentsourcetype === 'Election' && $row->governmentsourceapproveddate !== '') ? 'Certified ' : '') . $row->governmentsourceapproveddate : ($row->governmentsourcetype === 'Election' ? 'Rejected' : ($row->governmentsourcetype === 'Bill' ? '' : 'Veto' . ($row->governmentsourceapproveddate !== '' ? ' Overridden ' . $row->governmentsourceapproveddate : 'ed')))) ?>
+                            ? (($row->governmentsourcetype === 'Election' && $row->governmentsourceapproveddate !== '') ? lang('Application.certified') . ' ' : '') . $row->governmentsourceapproveddate : ($row->governmentsourcetype === 'Election' ? lang('Application.rejected') : ($row->governmentsourcetype === 'Bill' ? '' : lang('Application.veto') . ($row->governmentsourceapproveddate !== '' ? ' ' . lang('Application.overridden') . ' ' . $row->governmentsourceapproveddate : 'ed')))) ?>
                     </td>
                     <td data-sort="<?= $row->governmentsourceeffectivedatesort ?>"><?= $row->governmentsourceeffectivedate ?></td>
                     <td><?=
