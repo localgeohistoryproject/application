8a2f4a78798e571c7d77a8bd5abb6c8e4fdb0528
Mark A. Connelly <16519297+markconnellypro@users.noreply.github.com>
2026-01-10 17:30:12 -0500

    Source controller: Update with new language tags

--- app/Controllers/Source.php
+++ app/Controllers/Source.php
@@ -33,17 +33,17 @@ class Source extends BaseController
             $id = $query[0]->sourcecitationid;
             $pageTitle = $query[0]->sourceabbreviation . ($query[0]->sourcecitationpage === '' ? '' : ' ' . $query[0]->sourcecitationpage);
             echo view('core/header', ['title' => $this->title, 'pageTitle' => $pageTitle, 'url' => $url]);
-            echo view('source/table_citation', ['query' => $query, 'hasColor' => false, 'hasLink' => false, 'title' => 'Detail']);
+            echo view('source/table_citation', ['query' => $query, 'hasColor' => false, 'hasLink' => false, 'title' => lang('Application.detail')]);
             echo view('source/table', ['query' => $query, 'hasLink' => $this->isLive()]);
             if ($query[0]->url !== '') {
-                echo view('core/url', ['query' => $query, 'title' => 'Actual URL']);
+                echo view('core/url', ['query' => $query, 'title' => lang('Application.actualUrl')]);
             }
             $SourceCitationNoteModel = new SourceCitationNoteModel();
             echo view('source/note', ['query' => $SourceCitationNoteModel->getBySourceCitation($id)]);
             $SourceItemPartModel = new SourceItemPartModel();
-            echo view('core/url', ['query' => $SourceItemPartModel->getBySourceCitation($id), 'title' => 'Calculated URL']);
+            echo view('core/url', ['query' => $SourceItemPartModel->getBySourceCitation($id), 'title' => lang('Application.calculatedUrl')]);
             $EventModel = new EventModel();
-            echo view('event/table', ['query' => $EventModel->getBySourceCitation($id), 'title' => 'Event Links']);
+            echo view('event/table', ['query' => $EventModel->getBySourceCitation($id), 'title' => lang('Application.eventLinks')]);
             echo view('core/footer');
         }
     }
